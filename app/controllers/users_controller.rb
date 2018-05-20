94bfb072950b3bf64238dca88f7011eaca386ed4
Leandro Masello <leandro.masello@wolox.com.ar>
2018-05-20 18:51:57 -0300

    Add endpoint to get the logged-in user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   skip_before_action :current_user, :authenticate_request, only: [:create]
 
+  def logged_in
+    render json: current_user, status: :ok
+  end
+
   def create
     user = User.new(user_params)
     if user.save
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:create] do
     collection do
+      get :logged_in
       resources :sessions, only: [:create] do
         collection do
           post :renew
