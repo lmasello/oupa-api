6d27dd91998a73261cf7012510c5594b05ceee4e
Leandro Masello <leandro.masello@wolox.com.ar>
2018-05-29 15:49:03 -0300

    Add httparty for firebase requests

--- Gemfile
+++ Gemfile
@@ -38,6 +38,9 @@ gem 'jwt'
 # Authentication
 gem 'devise'
 
+# HTTP requests
+gem 'httparty'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/emergency_alarm_controller.rb
+++ app/controllers/emergency_alarm_controller.rb
@@ -2,11 +2,19 @@ class EmergencyAlarmController < ApplicationController
   def create
     return render json: { errors: error_message, status: :unprocessable_entity } if current_user.type == "AssistantUser"
     return render json: { errors: no_user_message, status: :unprocessable_entity } if current_user.assistant_users.empty?
+    firebase_url = 'https://fcm.googleapis.com/v1/projects/myproject-b5ae1/messages:send'
     current_user.assistant_users.each do |assistant_user|
       name = current_user.first_name + ' ' + current_user.last_name
       message_content = {
-        message: "Emergencia solicitada por #{name}!"
+        message: {
+          notification: {
+              title: 'Alarma',
+              body: "Emergencia solicitada por #{name}!"
+          },
+          token: assistant_user.device_token.to_s
+        }
       }
+      HTTParty.post(firebase_url, body: message_content.to_json, headers: { 'Content-Type' => 'application/json' } )
     end
     head :ok
   end
